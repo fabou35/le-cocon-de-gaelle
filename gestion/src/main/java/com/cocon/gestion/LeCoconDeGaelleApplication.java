d2011d69c2bd29e197670dfa84b44ffb3a38f398
Fabien De deken <fabien.dedeken@yahoo.fr>
2022-04-24 00:27:24 +0200

    initial commit

---
+++ gestion/src/main/java/com/cocon/gestion/LeCoconDeGaelleApplication.java
@@ -0,0 +1,13 @@
+package com.cocon.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeCoconDeGaelleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeCoconDeGaelleApplication.class, args);
+	}
+
+}
---
+++ gestion/src/test/java/com/cocon/gestion/LeCoconDeGaelleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cocon.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LeCoconDeGaelleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
